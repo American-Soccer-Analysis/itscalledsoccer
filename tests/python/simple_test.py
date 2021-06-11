053f2d51e64e9111fc512a80808e7e5523561bd2
bgrnwd <11201127+bgrnwd@users.noreply.github.com>
2021-06-10 21:49:40 -0400

    ASA-150: run python tests on push and create setup.py

--- tests/python/simple_test.py
+++
@@ -1,6 +0,0 @@
-from unittest import TestCase
-
-
-class PythonTest(TestCase):
-    def simple_test(self):
-        self.assertTrue(True)
---
+++ tests/python/test_simple.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestSimple(TestCase):
+    def test_simple(self):
+        pass

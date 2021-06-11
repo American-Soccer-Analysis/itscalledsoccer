e6de5321b6c07c37ab075bba878c5551f17fdfa4
bgrnwd <11201127+bgrnwd@users.noreply.github.com>
2021-06-10 21:49:40 -0400

    ASA-150: run python tests on push and create setup.py

---
+++ asa-package/core.py
@@ -0,0 +1,2 @@
+def solve_soccer():
+    print("Put the ball in the back of the net")
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="asa-package",
+    version="0.0.1",
+    description="Programmatically interact with the ASA API",
+    author="American Soccer Analysis",
+    url="https://github.com/American-Soccer-Analysis/asa-package",
+)

616602c946e8199f924e4b74a92ff59573486dcd
bgrnwd <11201127+bgrnwd@users.noreply.github.com>
2021-06-12 01:45:04 -0400

    ASA-150: add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -6,4 +6,12 @@ setup(
     description="Programmatically interact with the ASA API",
     author="American Soccer Analysis",
     url="https://github.com/American-Soccer-Analysis/asa-package",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    python_requires=">=3.6",
 )

6573a2067f7a9de50404c988dcaf9e320dab94fb
bgrnwd <11201127+bgrnwd@users.noreply.github.com>
2021-06-17 23:23:12 -0400

    ASA-150: remove python 3.5

--- python-package/setup.py
+++ python-package/setup.py
@@ -1,10 +1,15 @@
 from distutils.core import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
-    name="asa-package",
+    name="itscalledsoccer",
     version="0.0.1",
     description="Programmatically interact with the ASA API",
+    long_description=long_description,
     author="American Soccer Analysis",
+    author_email="",
     url="https://github.com/American-Soccer-Analysis/asa-package",
     classifiers=[
         "Programming Language :: Python",
@@ -14,4 +19,5 @@ setup(
         "Programming Language :: Python :: 3.8",
     ],
     python_requires=">=3.6",
+    keywords="stats soccer api american machine learning football",
 )

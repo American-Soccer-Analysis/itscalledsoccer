530ef8822e8cc7bc6536ee09887457a19225bc96
bgrnwd <11201127+bgrnwd@users.noreply.github.com>
2021-07-09 23:02:42 -0400

    ASA-150: consolidate class into 1 file

--- itscalledsoccer/managers.py
+++
@@ -1,9 +0,0 @@
-class Manager:
-    def __init__(self, manager_id: str, manager_name: str, nationality: str) -> None:
-        self.manager_id = manager_id
-        self.manager_name = manager_name
-        self.nationality = nationality
-
-
-class Managers:
-    pass

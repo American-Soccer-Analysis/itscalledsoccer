ab588517b2fb959e7c4ce27f6ee7ee917cbde302
bgrnwd <11201127+bgrnwd@users.noreply.github.com>
2021-11-24 23:20:27 -0500

    ASA-165: Add xPass methods and simple test

--- python-package/tests/features/steps/xgoals.py
+++ python-package/tests/features/steps/xgoals.py
@@ -1,11 +1,5 @@
 from behave import *
-from pandas import DataFrame
 
 @when("the get_player_xgoals function is called")
 def step_impl(context):
-    context.player_xgoals = context.soccer.get_player_xgoals(leagues="mls")
-
-@then("there is data")
-def step_impl(context):
-    assert context.player_xgoals is not None
-    assert isinstance(context.player_xgoals, DataFrame)
+    context.response = context.soccer.get_player_xgoals(leagues="mls")

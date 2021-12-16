3a94216ac27f3c5b974425b9b64d716b7c178c36
bgrnwd <11201127+bgrnwd@users.noreply.github.com>
2021-12-16 02:52:22 -0500

    ASA-181: Consolidate feature files and common steps, add tests for entities and logger

--- python-package/tests/features/steps/client.py
+++ python-package/tests/features/steps/client.py
@@ -1,9 +1,9 @@
 from behave import *
+from itscalledsoccer.client import AmericanSoccerAnalysis
 
 @then(u'the API_VERSION should be "{value}"')
 def step_impl(context, value):
     assert context.soccer.API_VERSION == value
-
 
 @then(u'the BASE_URL should be "{value}"')
 def step_impl(context, value):
@@ -13,8 +13,39 @@ def step_impl(context, value):
 def step_impl(context, value):
     assert context.soccer.MAX_API_LIMIT == int(value)
 
-
 @then(u'"{league}" should be in LEAGUES')
 def step_impl(context, league):
     print(context.soccer.LEAGUES)
     assert league in context.soccer.LEAGUES
+
+@then(u'there is a logger')
+def step_impl(context):
+    assert context.soccer.LOGGER is not None
+
+@then(u'the logging level is WARNING')
+def step_impl(context):
+    assert context.soccer.LOGGER.getEffectiveLevel() == 30
+
+@then(u'the logging level is DEBUG')
+def step_impl(context):
+    assert context.soccer.LOGGER.getEffectiveLevel() == 10
+
+@when(u'the logging level is set to "{level}"')
+def step_impl(context, level):
+    context.soccer = AmericanSoccerAnalysis(logging_level=level)
+
+@when(u'the proxy is set to "{url}"')
+def step_impl(context, url):
+    proxies = {
+        "http": f"http://{url}",
+        "https": f"https://{url}"
+    }
+    context.soccer = AmericanSoccerAnalysis(proxies=proxies)
+
+@then(u'the session is using a proxy')
+def step_impl(context):
+    proxies = {
+    'http': 'http://10.10.1.10:3128',
+    'https': 'http://10.10.1.10:1080',
+    }
+    assert context.soccer.SESSION.proxies == proxies
--- python-package/tests/features/steps/common_steps.py
+++ python-package/tests/features/steps/common_steps.py
@@ -1,6 +1,23 @@
 from behave import *
 from pandas import DataFrame
 
+def split_args(args: str) -> dict:
+    kwargs = {}
+    split = args.split("  ")
+    if len(split) > 1:
+        for s in split:
+            captured_args = s.split('=')
+            if "[" in captured_args[1]:
+                kwargs[captured_args[0]] = captured_args[1].strip("][").strip().split(",")
+            else:
+                kwargs[captured_args[0]] = captured_args[1]
+    else:
+        captured_args = split[0].split('=')
+        if "[" in captured_args[1]:
+            kwargs[captured_args[0]] = captured_args[1].strip("][").strip().split(",")
+        else:
+            kwargs[captured_args[0]] = captured_args[1]
+    return kwargs
 
 @given("there is an ASA client")
 def step_impl(context):
@@ -11,3 +28,41 @@ def step_impl(context):
     assert context.response is not None
     assert isinstance(context.response, DataFrame)
     assert len(context.response) >= 1
+
+@when('the "{function}" function is called with arguments "{args}"')
+def step_impl(context, function, args):
+    kwargs = split_args(args)
+    print(kwargs)
+    if "get_player_xgoals" == function:
+        context.response = context.soccer.get_player_xgoals(**kwargs)
+    elif "get_player_xpass" == function:
+        context.response = context.soccer.get_player_xpass(**kwargs)
+    elif "get_player_goals_added" == function:
+        context.response = context.soccer.get_player_goals_added(**kwargs)
+    elif "get_goalkeeper_xgoals" == function:
+        context.response = context.soccer.get_goalkeeper_xgoals(**kwargs)
+    elif "get_goalkeeper_goals_added" == function:
+        context.response = context.soccer.get_goalkeeper_goals_added(**kwargs)
+    elif "get_team_xgoals" == function:
+        context.response = context.soccer.get_team_xgoals(**kwargs)
+    elif "get_team_xpass" == function:
+        context.response = context.soccer.get_team_xpass(**kwargs)
+    elif "get_team_goals_added" == function:
+        context.response = context.soccer.get_team_goals_added(**kwargs)
+    elif "get_game_xgoals" == function:
+        context.response = context.soccer.get_game_xgoals(**kwargs)
+    elif "get_stadia" == function:
+        context.response = context.soccer.get_stadia(**kwargs)
+    elif "get_referees" == function:
+        context.response = context.soccer.get_referees(**kwargs)
+    elif "get_managers" == function:
+        context.response = context.soccer.get_managers(**kwargs)
+    elif "get_teams" == function:
+        context.response = context.soccer.get_teams(**kwargs)
+    elif "get_players" == function:
+        context.response = context.soccer.get_players(**kwargs)
+    elif "get_games" == function:
+        context.response = context.soccer.get_games(**kwargs)
+    else:
+        print("Function not recognized")
+        raise NameError

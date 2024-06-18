27c569af72baf89c4a61daef5b862498fe1dd3fc
bgrnwd <11201127+bgrnwd@users.noreply.github.com>
2024-06-17 22:28:24 -0400

    chore: delete features directory

--- tests/features/environment.py
+++
@@ -1,7 +0,0 @@
-from itscalledsoccer.client import AmericanSoccerAnalysis
-from unittest.mock import patch
-
-
-@patch("itscalledsoccer.client.AmericanSoccerAnalysis._get_entity")
-def before_all(context, mock_entity):
-    context.soccer = AmericanSoccerAnalysis()
